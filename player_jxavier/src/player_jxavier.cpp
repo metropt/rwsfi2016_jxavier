47ea8d268f965b73d47806d77b8e403c65e9ede7
Jose Xavier <josescxavier@gmail.com>
2016-12-21 13:01:43 +0000

    teste

--- player_jxavier/src/player_jxavier.cpp
+++ player_jxavier/src/player_jxavier.cpp
@@ -12,29 +12,44 @@
 using namespace std;
 using namespace ros;
 
+string playerToKill;
+
 
 /**
  * @brief MyPlayer extends class Player, i.e., there are additional things I can do with MyPlayer and not with any Player, e.g., to order a movement.
  */
 class MyPlayer: public rwsfi2016_libs::Player
 {
-  public: 
+public:
 
-    /**
+  /**
      * @brief Constructor, nothing to be done here
      * @param name player name
      * @param pet_name pet name
      */
-    MyPlayer(string player_name, string pet_name="/dog"): Player(player_name, pet_name){};
+  MyPlayer(string player_name, string pet_name="/dog"): Player(player_name, pet_name){};
 
-    void play(const rwsfi2016_msgs::MakeAPlay& msg)
-    {
-      //Custom play behaviour. Now I will win the game
+  void play(const rwsfi2016_msgs::MakeAPlay& msg)
+  {
+    //Custom play behaviour. Now I will win the game
+    string tmp;
+    if(playerToKill.compare("")==0)
+      playerToKill = msg.green_alive.at(0);
 
-      //Behaviour follow the closest prey
-      move(msg.max_displacement, ((((double)rand()/(double)RAND_MAX) ) * 0.5 -1) + getAngleToPLayer(preys_team->players[0]) );
-      //cout << ((((double)rand()/(double)RAND_MAX) ) * 2 -1) << endl;
+    int test = 0;
+    for(int i=0;i<3;i++){
+      if(playerToKill.compare(msg.green_alive.at(i)) == 0){
+        test = 1;
+      }
     }
+
+    if(!test)
+      playerToKill = msg.green_alive.at(0);
+
+    //Behaviour follow the closest prey
+    move(msg.max_displacement, getAngleToPLayer(playerToKill)  );
+    //cout << ((((double)rand()/(double)RAND_MAX) ) * 2 -1) << endl;
+  }
 };
 
 
@@ -51,6 +66,7 @@ int main(int argc, char** argv)
   // ------------------------
   string my_name = "jxavier";
   string my_pet = "/turtle";
+  playerToKill = "";
 
   //initialize ROS stuff
   ros::init(argc, argv, my_name);
