5083a36633c102614aed4ae5c0edbd3064b4cdc8
Jose Xavier <josescxavier@gmail.com>
2016-12-21 11:46:09 +0000

    init player

---
+++ player_jxavier/src/player_jxavier.cpp
@@ -0,0 +1,62 @@
+/* _________________________________
+   |                                 |
+   |           INCLUDES              |
+   |_________________________________| */
+#include <ros/ros.h>
+#include <rwsfi2016_libs/player.h>
+
+/* _________________________________
+   |                                 |
+   |              CODE               |
+   |_________________________________| */
+using namespace std;
+using namespace ros;
+
+
+/**
+ * @brief MyPlayer extends class Player, i.e., there are additional things I can do with MyPlayer and not with any Player, e.g., to order a movement.
+ */
+class MyPlayer: public rwsfi2016_libs::Player
+{
+  public: 
+
+    /**
+     * @brief Constructor, nothing to be done here
+     * @param name player name
+     * @param pet_name pet name
+     */
+    MyPlayer(string player_name, string pet_name="/dog"): Player(player_name, pet_name){};
+
+    void play(const rwsfi2016_msgs::MakeAPlay& msg)
+    {
+      //Custom play behaviour. Now I will win the game
+
+      //Behaviour follow the closest prey
+      move(msg.max_displacement, 0.5);
+    }
+};
+
+
+/**
+ * @brief The main function. All you need to do here is enter your name and your pets name
+ * @param argc number of command line arguments
+ * @param argv values of command line arguments
+ * @return result
+ */
+int main(int argc, char** argv)
+{
+  // ------------------------
+  //Replace this with your name
+  // ------------------------
+  string my_name = "jxavier";
+  string my_pet = "/turtle";
+
+  //initialize ROS stuff
+  ros::init(argc, argv, my_name);
+
+  //Creating an instance of class MyPlayer
+  MyPlayer my_player(my_name, my_pet);
+
+  //Infinite spinning (until ctrl-c)
+  ros::spin();
+}
